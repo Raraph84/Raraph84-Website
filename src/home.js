6d026113439c698dc64c2de4c954a701b37a535e
Raraph84 <contact@raraph.fr>
2022-07-15 21:56:21 +0200

    Edit dgroup desc

--- src/home.js
+++ src/home.js
@@ -22,7 +22,7 @@ export class Home extends Component {
             <div className="subtitle">Mes projets</div>
             <div className="projects">
                 <Project link="https://eclipsia.fr/" name="Eclipsia" imageUrl="/imgs/projects/logo-eclipsia.png" description="Eclipsia est un serveur Minecraft Mini-Jeux créé par des amis." />
-                <Project link="https://discord.gg/AKMzGb9" name="DommiossGroup" imageUrl="/imgs/projects/logo-dommioss-group.png" description="Le DommiossGroup est un groupe de projets web et Minecraft fondé en 2018, développant actuellement un CMS et un serveur Minecraft Roleplay." />
+                <Project link="https://discord.gg/AKMzGb9" name="Groupe Dommioss" imageUrl="/imgs/projects/logo-dommioss-group.png" description="Dommioss est un groupe de développeurs fondé en 2018 travaillants sur divers projets tels que des serveurs Minecraft ou encore le CMS DomCord." />
                 <Project link="https://discord.gg/5vZxSUy" name="Cubik Market" imageUrl="/imgs/projects/logo-cubik-market.png" description="Le Cubik Market est un market pour le serveur Minecraft Paladium entièrement réalisé et automatisé sur Discord." />
                 <Project link="https://discord.gg/HQKUWDe" name="Discord Colonies" imageUrl="/imgs/projects/logo-discord-colonies.png" description="Discord Colonies est un serveur Discord où l'on peut créer son pays, farmer des ressources, coloniser etc." />
                 <Project link="https://discord.gg/GWFzTcN" name="Pala's Fun" imageUrl="/imgs/projects/logo-palas-fun.png" description="Pala's Fun est un Bot Discord en rapport avec le serveur Minecraft Paladium." />
